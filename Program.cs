57a62e94d580dec59ab767b4cca23cd8ee952d01
akudepe <akudepe@LAPTOP-D47KPSQ0>
2026-03-25 04:33:39 +0700

    fix cors production

--- Program.cs
+++ Program.cs
@@ -22,9 +22,12 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://127.0.0.1:5500")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
+            policy.WithOrigins(
+     "http://127.0.0.1:5500",
+     "https://inventory-app.netlify.app"
+ )
+ .AllowAnyHeader()
+ .AllowAnyMethod();
         });
 });
 
